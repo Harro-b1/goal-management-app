6fed7211a308e52b1928c772d3cb3014bfc2eff2
Harro-b1 <harrobird78@gmail.com>
2026-07-21 14:48:44 +1000

    Added single get by id API for types and goals

--- src/main/java/com/harro/goaltracker/controllers/GoalController.java
+++ src/main/java/com/harro/goaltracker/controllers/GoalController.java
@@ -2,7 +2,9 @@ package com.harro.goaltracker.controllers;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +23,19 @@ public class GoalController {
     
     private final GoalMapper goalMapper;
 
-    @GetMapping List<GoalDto> getAllGoals(){
+    @GetMapping 
+    public List<GoalDto> getAllGoals(){
         List<Goal> goals = goalRepository.findAll();
 
         return goals.stream().map(goalMapper::toDto).toList();
+    }
+
+    @GetMapping("/{id}") ResponseEntity<GoalDto> getGoal(@PathVariable Long id){
+        var goal = goalRepository.findById(id).orElse(null);
+        if (goal == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(goalMapper.toDto(goal));
     }
 }
--- src/main/java/com/harro/goaltracker/controllers/TypeController.java
+++ src/main/java/com/harro/goaltracker/controllers/TypeController.java
@@ -2,7 +2,9 @@ package com.harro.goaltracker.controllers;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,16 @@ public class TypeController {
         List<Type> types = typeRepository.findAll();
 
         return types.stream().map(typeMapper::toDto).toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TypeDto> getType(@PathVariable Long id){
+        var type = typeRepository.findById(id).orElse(null);
+
+        if(type == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(typeMapper.toDto(type));
     }
 }
