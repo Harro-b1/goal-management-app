19d13a4a9c7d26f8767b2f380d861ab5c6b55962
Harro-b1 <harrobird78@gmail.com>
2026-07-22 11:20:41 +1000

    Created goal complete and uncomplete API endpoints

--- src/main/java/com/harro/goaltracker/controllers/GoalController.java
+++ src/main/java/com/harro/goaltracker/controllers/GoalController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.harro.goaltracker.Repositories.GoalRepository;
+import com.harro.goaltracker.Repositories.TypeRepository;
 import com.harro.goaltracker.mappers.GoalMapper;
 import com.harro.goaltracker.dtos.GoalDto;
 import com.harro.goaltracker.entities.Goal;
@@ -20,6 +22,7 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/goals")
 public class GoalController {
     private final GoalRepository goalRepository;
+    private final TypeRepository typeRepository;
     
     private final GoalMapper goalMapper;
 
@@ -30,11 +33,42 @@ public class GoalController {
         return goals.stream().map(goalMapper::toDto).toList();
     }
 
-    @GetMapping("/{id}") ResponseEntity<GoalDto> getGoal(@PathVariable Long id){
+    @GetMapping("/{id}") 
+    public ResponseEntity<GoalDto> getGoal(@PathVariable Long id){
         var goal = goalRepository.findById(id).orElse(null);
         if (goal == null){
             return ResponseEntity.notFound().build();
         }
+
+        return ResponseEntity.ok(goalMapper.toDto(goal));
+    }
+
+    @PutMapping("/{id}/complete")
+    public ResponseEntity<GoalDto> completeJob(
+        @PathVariable(name = "id") Long id
+    ){
+        var goal = goalRepository.findById(id).orElse(null);
+        
+        if(goal == null){
+            return ResponseEntity.notFound().build();
+        }
+        goal.setCompleted(true);
+        goalRepository.save(goal);
+
+        return ResponseEntity.ok(goalMapper.toDto(goal));
+    }
+
+    @PutMapping("/{id}/uncomplete")
+    public ResponseEntity<GoalDto> uncompleteJob(
+        @PathVariable(name = "id") Long id
+    ){
+        var goal = goalRepository.findById(id).orElse(null);
+        
+        if(goal == null){
+            return ResponseEntity.notFound().build();
+        }
+        goal.setCompleted(false);
+        goalRepository.save(goal);
 
         return ResponseEntity.ok(goalMapper.toDto(goal));
     }
